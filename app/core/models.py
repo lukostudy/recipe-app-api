7dd645b527a35844a6ba809c503cda30244628c3
lukostudy <lukostudy@gmail.com>
2023-12-18 11:31:32 +0100

    Added Create User Model (completed s9)
    Model for creating users has been added.
    Now it's possible to create users with the administration panel
    localhost:8000/admin
    
    Before logging a superuser is required, that can be created with:
    python manage.py createsuperuser

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,50 @@
-from django.db import models  # noqa
+"""
+Dababase  models.
+"""
 
-# Create your models here.
+
+from django.db import models  # noqa
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user in the system
+        used by various Django modules -> name must be spelled correctly
+        """
+        if not email:
+            raise ValueError("User must provide an email address")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser
+        used by various Django modules -> name must be spelled correctly
+        """
+        user = self.create_user(email=email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    # this specifies whicj field is treated as a username
+    # by default it would be username
+    USERNAME_FIELD = 'email'
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,51 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model  # helper function
+
+
+class ModelTests(TestCase):
+    """Test users models"""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
+        email = "test@example.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        # checking password with a method that uses hashing
+        # password should be stored hashed
+        # this must be implemented
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """test email is normalized for new users"""
+        sample_emails = [
+            ['test1@EXAMPLE.COM', 'test1@example.com'],
+            ['Test1@EXAmple.COM', 'Test1@example.com'],
+            ['JohnSmith@example.com', 'JohnSmith@example.com'],
+            ['hswr3847Q@Example.com', 'hswr3847Q@example.com'],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test if a new user without an email raises ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
